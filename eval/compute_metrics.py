e611160d9088d6a5a57aba58c832d27e31c7907f
Piotr Teterwak <piotrt@koller.bu.edu>
2021-04-26 19:58:55 -0400

    Add laplacian eval metric

---
+++ eval/compute_metrics.py
@@ -0,0 +1,23 @@
+import cv2
+from glob import glob
+import os
+
+ground_truth_imgs = sorted(x for x in glob('../deblur_gan/results/ground_truth/*'))
+deblurred_imgs = sorted(x for x in glob('../deblur_gan/results/deblurred/*'))
+
+metric = []
+
+for imgs in zip(ground_truth_imgs,deblurred_imgs):
+    gt_img_path, deblurred_img_path = imgs
+    gt = cv2.imread(gt_img_path)
+    db = cv2.imread(deblurred_img_path)
+    gray_gt = cv2.cvtColor(gt, cv2.COLOR_BGR2GRAY)
+    gray_db = cv2.cvtColor(db, cv2.COLOR_BGR2GRAY)
+    gt_metric = cv2.Laplacian(gray_gt, cv2.CV_64F).var()
+    db_metric = cv2.Laplacian(gray_db, cv2.CV_64F).var()
+    metric.append(db_metric - gt_metric)
+
+
+average_metric = sum(metric)/len(metric)
+
+print('Average Improvement: {}'.format(average_metric))
