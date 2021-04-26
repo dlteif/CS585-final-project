22734cc4f4c20c7018e968280e601941a3b84c50
Piotr Teterwak <piotrt@koller.bu.edu>
2021-04-26 15:23:58 -0400

    Adding DeblurGANv2

---
+++ deblur_gan/setup_deblurgan.sh
@@ -0,0 +1,9 @@
+pip install  -r requirements.txt
+
+git clone https://github.com/VITA-Group/DeblurGANv2.git
+
+mkdir data
+mkdir results
+
+wget https://cs-people.bu.edu/piotrt/data/recycle.mp4 -O data/recycle.mp4
+wget https://cs-people.bu.edu/piotrt/data/fpn_inception.h5 -O data/fpn_inception.h5
